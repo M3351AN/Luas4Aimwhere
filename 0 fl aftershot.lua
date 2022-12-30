9b453b9bc1fc4da0637287ace0dafd11103b6247
渟雲 <QuQ@outlook.it>
2022-12-30 21:52:04 +0800

    A.K.A. smfl but idk why

---
+++ 0 fl aftershot.lua
@@ -0,0 +1,32 @@
+--ich forgot my aimware password so ne tested/(ToT)/~~
+--da, ia ist m1tZw desu!!
+client.AllowListener("weapon_fire")--aimwhere...
+local lP = entities.GetLocalPlayerIndex()
+local shotFl = gui.Checkbox(m1tZw,"shotFl", "0 fl on shot ", false)
+--local shotflticks = gui.Slider(m1tZw,"shotflticks", "0 fl on shot ticks",0,0,128)
+--sie sagen subete 15 ticks...
+local reset = 0
+local shotTime = 0
+local originFl = 0
+callbacks.Register("createmove", "timer",function()
+    if reset == 1 and shotTime < globals.TickCount() then 
+        gui.SetValue("misc.fakelag.factor",originFl)--reset fl
+        reset = 0
+    else if reset == 1 and not (shotTime < globals.TickCount()) then--anti other fl luas.15ticks such long long a time right?
+        gui.SetValue("misc.fakelag.factor",0)--keep fl 0 
+    end
+end)
+local function smFl(Event)--所以说smfl是什么意思，虐待假卡？😳
+    if (event:Get_Name("weapon_fire")) and gui.GetValue("shotFl") then
+        local fireman = Event:GetInt("userid");
+        local firemanindex = client.GetPlayerNameByUserID(fireman);
+        if (firemanindex == lP)  then
+            originFl = gui.GetValue("misc.fakelag.factor")--fetch origin fl
+            gui.SetValue("misc.fakelag.factor",0)--set fl 0
+            shotTime = globals.TickCount() + 15--kept for 15ticks or you can set to gui.GetValue("shotflticks")
+            reset = 1
+        end
+
+    end
+end
+callbacks.Register("FireGameEvent", "smFl", smFl);
