565978c56e3d71ce419602cbf5a057acfe99af3d
渟雲 <QuQ@outlook.it>
2023-01-02 17:03:56 +0800

    fix non correct origin fakelag reset

--- 0 fl aftershot.lua
+++ 0 fl aftershot.lua
@@ -12,7 +12,7 @@ callbacks.Register("createmove", "timer",function()
     if reset == 1 and shotTime < globals.TickCount() then 
         gui.SetValue("misc.fakelag.factor",originFl)--reset fl
         reset = 0
-    else if reset == 1 and not (shotTime < globals.TickCount()) then--anti other fl luas.15ticks such long long a time right?
+    else if reset == 1 and shotTime > globals.TickCount() then--anti other fl luas.15ticks such long long a time right?
         gui.SetValue("misc.fakelag.factor",0)--keep fl 0 
     end
 end)
@@ -20,11 +20,17 @@ local function smFl(Event)--所以说smfl是什么意思，虐待假卡？😳
     if (event:Get_Name("weapon_fire")) and gui.GetValue("shotFl") then
         local fireman = Event:GetInt("userid");
         local firemanindex = client.GetPlayerNameByUserID(fireman);
-        if (firemanindex == lP)  then
+        if firemanindex == lP and reset == 0 then
             originFl = gui.GetValue("misc.fakelag.factor")--fetch origin fl
             gui.SetValue("misc.fakelag.factor",0)--set fl 0
             shotTime = globals.TickCount() + 15--kept for 15ticks or you can set to gui.GetValue("shotflticks")
             reset = 1
+            return
+        end
+        if firemanindex == lP and reset == 1 then
+            gui.SetValue("misc.fakelag.factor",0)--set fl 0
+            shotTime = globals.TickCount() + 15--kept for 15ticks or you can set to gui.GetValue("shotflticks")
+            return
         end
 
     end
